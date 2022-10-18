2857840eb425b99bb4e8010d14f0099d809ef86b
riverback <hhz_pku@163.com>
2022-10-18 15:12:08 +0800

    init encoder decoder

--- resnet_models.py
+++ resnet_models.py
@@ -303,10 +303,94 @@ def resnet152(in_channels, num_classes, down='True', **kwargs) -> ResNet:
     return _resnet(Bottleneck, [3, 8, 36, 3], in_channels, num_classes, down=down, **kwargs)
 
 
+
+
+class DO_Encoder_Decoder(nn.Module):
+    def __init__(self, block: Type[Union[BasicBlock, Bottleneck]], layers: List[int], in_channels, num_classes):
+        super().__init__()
+        
+        self.in_channels = in_channels
+        self.num_classes = num_classes
+        self.inplanes = 64
+        self.dilation = 1
+        self.groups = 1
+        self.base_width = 64
+
+        
+        self.layer0 = nn.Sequential(
+            nn.Conv2d(self.in_channels, self.inplanes, kernel_size=7, stride=2, padding=3, bias=False),
+            nn.BatchNorm2d(self.inplanes),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+        )
+        
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=1, )
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=1, )
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=1, )
+        
+        # self.upconv4 = nn.ConvTranspose2d(in_channels=512, out_channels=256, kernel_size=3, stride=2, padding=1)
+        # self.upconv3 = nn.ConvTranspose2d(in_channels=512, out_channels=256, kernel_size=3, stride=2, padding=1)
+        
+        
+    
+    def forward(self, x:Tensor)->Tensor:
+        x = self.layer0(x)
+        
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        return x
+        
+    def _make_layer(
+        self,
+        block: Type[Union[BasicBlock, Bottleneck]],
+        planes: int,
+        blocks: int,
+        stride: int = 1,
+        dilate: bool = False,
+    ) -> nn.Sequential:
+        norm_layer = nn.BatchNorm2d
+        downsample = None
+        previous_dilation = self.dilation
+        if dilate:
+            self.dilation *= stride
+            stride = 1
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                norm_layer(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(
+            block(
+                self.inplanes, planes, stride, downsample, self.groups, self.base_width, previous_dilation, norm_layer
+            )
+        )
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(
+                block(
+                    self.inplanes,
+                    planes,
+                    groups=self.groups,
+                    base_width=self.base_width,
+                    dilation=self.dilation,
+                    norm_layer=norm_layer,
+                )
+            )
+
+        return nn.Sequential(*layers)
+
+
 class BasicNet(nn.Module):
     def __init__(self, in_channels, num_classes, down):
         super(BasicNet, self).__init__()
         self.extractor = ResNet(BasicBlock, [3,4,6,3], in_channels, num_classes, down=down)
+        # self.extractor = DO_Encoder_Decoder(BasicBlock, [3, 4, 6, 3], in_channels, num_classes)
         self.pool = nn.AdaptiveAvgPool2d(1)
         self.classifier = nn.Conv2d(512, num_classes, 1, 1, padding=0)
     
@@ -325,7 +409,8 @@ class BasicNet(nn.Module):
     
     def load_pretrained(self, checkpoint_path):
         self.load_state_dict(torch.load(checkpoint_path))
-    
+
+
 __all__ = [BasicNet]
     
 if __name__ == '__main__':
